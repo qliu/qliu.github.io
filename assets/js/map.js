1db8b9c1dde9849b8ba67e525671016da9e8f2cf
qliu <Qing.Liu@du.edu>
2015-03-24 23:40:47 -0600

    add projects to homepage. change map settings

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,6 +4,7 @@ map.worldCopyJump = true;
 var basemap = L.tileLayer(
 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
 maxZoom: 18,
+minZoom: 3,
 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
